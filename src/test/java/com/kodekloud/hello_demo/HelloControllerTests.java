1cbea2aa92bb2367a5fec6a0ab63880c3c3759b8
Lakchouch Anas <lakchouchanas@gmail.com>
2026-01-04 14:29:36 +0100

    Update HelloControllerTests.java

--- src/test/java/com/kodekloud/hello_demo/HelloControllerTests.java
+++ src/test/java/com/kodekloud/hello_demo/HelloControllerTests.java
@@ -60,7 +60,7 @@ public class HelloControllerTests {
     public void welcome_startsWithExpectedGreeting() throws Exception {
       mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
           .andExpect(status().isOk())
-          .andExpect(content().string(startsWith("Hola"))); 
+          .andExpect(content().string(startsWith("Hello"))); 
 
     }
 
@@ -71,4 +71,5 @@ public class HelloControllerTests {
           .andExpect(content().string(endsWith("community!")));
 
     }
+
   }
